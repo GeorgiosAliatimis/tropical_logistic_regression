e4eff92508be8b801392ea8a0bc418ed6b53aeba
GeorgiosAliatimis <g.aliatimis@lancaster.ac.uk>
2023-05-20 20:36:47 +0100

    refactoring & comparison to species tree from paper

--- lungfish/lungfish_tropical_logistic_regression.R
+++ lungfish/lungfish_tropical_logistic_regression.R
@@ -10,416 +10,162 @@
 #############################################
 
 # New library for calculating ROC curves
+rm(list=ls())
 library(ROCR)
-source("load_data.R")
+source("coalescent_model/load_data.R")
 source("test_model.R")
-source("methods/ulr.R")
+source("lungfish/train_and_test.R")
 
 ## NJ trees
 ## Reading lungfish data
-n <- 10 ## number of leaves
-T <- ape::read.tree("data/lungfish_nj.txt") ## reading the input file of trees
-N <- length(T) ## sample size of the data set
-D <- matrix(rep(0, N*choose(n, 2)), N, choose(n, 2))
-L <- T[[1]]$tip.label
 
-for(i in 1:N){
-    # u <- force.ultrametric(T[[i]], "nnls")
-    u <- T[[i]]
-    u$edge.length <- u$edge.length/max(u$edge.length)
-    D0 <- cophenetic(u)[L, ]
-    D[i, ] <- D0[lower.tri(t(D0))]
+set.seed(12345)
+dir = "lungfish/data/"
+
+tree_files = c("lungfish_nj.txt","lungfish_mle.txt")
+methods = c("NJ","MLE")
+
+class_files = c("clustering_ncut_direct_cls2.txt",
+                "clustering_ncut_tsne_D2_cls2.txt",
+                "clustering_ncut_kpca_D2_cls2.txt",
+                "clustering_ncut_tsne_D3_cls2.txt",
+                "clustering_ncut_kpca_D3_cls2.txt")
+classes = c("direct","TSNE1","kpca1","TSNE2","kpca2")
+
+rocs = list()
+aucs = list()
+pars = list()
+for(i in 1:length(tree_files)){
+  tree_file = paste(dir,tree_files[i],sep="")
+  method = methods[i]
+  class_file_pref = paste(dir,"fish_",method,"_",sep="")
+  for(j in 1:length(class_files)){
+    class_file = paste(class_file_pref,class_files[i],sep="")
+    class = classes[j]
+    res = train_and_test(tree_file,class_file,model="tlr")
+    key = paste(method,".",class,sep="")
+    rocs[[key]] = res$ROC
+    aucs[key] = res$AUC
+    pars[[key]] = res$pars
+  }
 }
-
-set.seed(876)
-
-
-
-
-### NCUT with direct with L_2 metric
-
-### Reading response variable
-YY <- read.table("data/fish_NJ_clustering_ncut_direct_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.NJ.direct <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.NJ.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.NJ.direct <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.NJ.kpca
-
-
-
-
-### NCUT with TSNE with L_2 metric
-
-### Reading response variable
-YY <- read.table("data/fish_NJ_clustering_ncut_tsne_D2_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.NJ.TSNE1 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.NJ.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.NJ.TSNE1 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.NJ.kpca
-
-
-
-
-### NCUT with KPCA with L_2 metric
-
-### Reading response variable
-YY <- read.table("data/fish_NJ_clustering_ncut_kpca_D2_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.NJ.kpca1 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.NJ.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.NJ.kpca1 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.NJ.kpca
-
-
-
-
-### NCUT with TSNE with BHV metric
-
-### Reading response variable
-YY <- read.table("data/fish_NJ_clustering_ncut_tsne_D3_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.NJ.TSNE2 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.NJ.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.NJ.TSNE2 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.NJ.kpca
-
-
-
-
-### NCUT with KPCA with BHV metric
-
-### Reading response variable
-YY <- read.table("data/fish_NJ_clustering_ncut_kpca_D3_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.NJ.kpca2 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.NJ.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.NJ.kpca2 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.NJ.kpca
-
-
-#####  MLE tree
-
-## Reading lungfish data
-n <- 10 ## number of leaves
-T <- ape::read.tree("data/lungfish_mle.txt") ## reading the input file of trees
-N <- length(T) ## sample size of the data set
-D <- matrix(rep(0, N*choose(n, 2)), N, choose(n, 2))
-L <- T[[1]]$tip.label
-
-for(i in 1:N){
-    # u <- force.ultrametric(T[[i]], "nnls")
-    u <- T[[i]]
-    u$edge.length <- u$edge.length/max(u$edge.length)
-    D0 <- cophenetic(u)[L, ]
-    D[i, ] <- D0[lower.tri(t(D0))]
-}
-
-set.seed(876)
-
-
-
-
-### NCUT with direct with L_2 metric
-
-### Reading response variable
-YY <- read.table("data/fish_MLE_clustering_ncut_direct_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.MLE.direct <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.MLE.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.MLE.direct <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.MLE.kpca
-
-
-
-
-### NCUT with TSNE with L_2 metric
-
-### Reading response variable
-YY <- read.table("data/fish_MLE_clustering_ncut_tsne_D2_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.MLE.TSNE1 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.MLE.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.MLE.TSNE1 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.MLE.kpca
-
-
-
-
-### NCUT with KPCA with L_2 metric
-
-### Reading response variable
-YY <- read.table("data/fish_MLE_clustering_ncut_kpca_D2_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.MLE.kpca1 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.MLE.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.MLE.kpca1 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.MLE.kpca
-
-
-
-
-### NCUT with TSNE with BHV metric
-
-### Reading response variable
-YY <- read.table("data/fish_MLE_clustering_ncut_tsne_D3_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.MLE.TSNE2 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.MLE.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.MLE.TSNE2 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.MLE.kpca
-
-
-
-
-### NCUT with KPCA with BHV metric
-
-### Reading response variable
-YY <- read.table("data/fish_MLE_clustering_ncut_kpca_D3_cls2.txt", header = FALSE, sep = " ", dec = ".")
-
-sampler <- sample(nrow(D),trunc(nrow(D)*.80)) # samples index 
-D.train <- D[sampler,]
-D.test <- D[-sampler,]
-
-Y.train <- YY[sampler, 1]
-Y.test <- YY[-sampler, 1]
-
-res <- logistic(Y.train, D.train)
-res
-
-Y.hat <- Testing.Model(res[[2]], D.test)
-Y.class <- ifelse(Y.hat > 0.5, 1, 0)
-
-# Calculate ROC statistics for our best logit model
-Logit.ROC.MLE.kpca2 <- performance(prediction(Y.hat, Y.test), measure="tpr", x.measure="fpr")
-#plot(Logit.ROC.MLE.kpca, lwd = 2, main = "ROC Curve for Logistic Regression Model")
-
-# Calculate AUC - use @y.values to get AUC out
-Logit.AUC.MLE.kpca2 <- performance(prediction(Y.hat, Y.test), 
-                         measure="auc")@y.values
-#Logit.AUC.MLE.kpca
-
 
 ### NJ
 ####  ROC
 # Add direct
-png("ROC_lungfish_NJ.png",width = 420, height = 420,)
-plot(Logit.ROC.NJ.direct, lwd=2, main = "ROC for Tropical Logistic Regression with NJ")
+# png("ROC_lungfish_NJ.png",width = 420, height = 420,)
+plot(rocs$NJ.direct, lwd=2, main = "ROC for Tropical Logistic Regression with NJ")
 
 # Add TNSE with L2
-lines(attributes(Logit.ROC.NJ.TSNE1)$x.values[[1]], attributes(Logit.ROC.NJ.TSNE1)$y.values[[1]], 
+lines(attributes(rocs$NJ.TSNE1)$x.values[[1]], attributes(rocs$NJ.TSNE1)$y.values[[1]], 
       col="red", lwd=2)
 
 # Add KPCA with L2
-lines(attributes(Logit.ROC.NJ.kpca1)$x.values[[1]], attributes(Logit.ROC.NJ.kpca1)$y.values[[1]], 
+lines(attributes(rocs$NJ.kpca1)$x.values[[1]], attributes(rocs$NJ.kpca1)$y.values[[1]], 
       col="blue", lwd=2)
 
 # Add TNSE with BHV
-lines(attributes(Logit.ROC.NJ.TSNE2)$x.values[[1]], attributes(Logit.ROC.NJ.TSNE2)$y.values[[1]], 
+lines(attributes(rocs$NJ.TSNE2)$x.values[[1]], attributes(rocs$NJ.TSNE2)$y.values[[1]], 
       col="green", lwd=2)
 
 # Add KPCA with BHV
-lines(attributes(Logit.ROC.NJ.kpca2)$x.values[[1]], attributes(Logit.ROC.NJ.kpca2)$y.values[[1]], 
+lines(attributes(rocs$NJ.kpca2)$x.values[[1]], attributes(rocs$NJ.kpca2)$y.values[[1]], 
       col="brown", lwd=2)
 
 
 #Add Legend
 legend(x=.6, y=.4, c("Direct", "TNSE w/ L2", "KPCA w/ L2", "TNSE w/ BHV", "KPCA w/ BHV"), 
        col=c("black", "red", "blue", "green", "brown"), lwd=c(2,2,2,2,2,2))
-dev.off()
+# dev.off()
 #### AUC
-Logit.AUC.NJ.direct
-Logit.AUC.NJ.TSNE1
-Logit.AUC.NJ.kpca1
-Logit.AUC.NJ.TSNE2
-Logit.AUC.NJ.kpca2
+print(aucs$NJ.direct)
+print(aucs$NJ.TSNE1)
+print(aucs$NJ.kpca1)
+print(aucs$NJ.TSNE2)
+print(aucs$NJ.kpca2)
 
 #### MLE
 ####  ROC
 # Add direct
-png("ROC_lungfish_MLE.png",width = 420, height = 420,)
-plot(Logit.ROC.MLE.direct, lwd=2, main = "ROC for Tropical Logistic Regression with MLE")
+# png("ROC_lungfish_MLE.png",width = 420, height = 420,)
+plot(rocs$MLE.direct, lwd=2, main = "ROC for Tropical Logistic Regression with MLE")
 
 # Add TNSE with L2
-lines(attributes(Logit.ROC.MLE.TSNE1)$x.values[[1]], attributes(Logit.ROC.MLE.TSNE1)$y.values[[1]], 
+lines(attributes(rocs$MLE.TSNE1)$x.values[[1]], attributes(rocs$MLE.TSNE1)$y.values[[1]], 
       col="red", lwd=2)
 
 # Add KPCA with L2
-lines(attributes(Logit.ROC.MLE.kpca1)$x.values[[1]], attributes(Logit.ROC.MLE.kpca1)$y.values[[1]], 
+lines(attributes(rocs$MLE.kpca1)$x.values[[1]], attributes(rocs$MLE.kpca1)$y.values[[1]], 
       col="blue", lwd=2)
 
 # Add TNSE with BHV
-lines(attributes(Logit.ROC.MLE.TSNE2)$x.values[[1]], attributes(Logit.ROC.MLE.TSNE2)$y.values[[1]], 
+lines(attributes(rocs$MLE.TSNE2)$x.values[[1]], attributes(rocs$MLE.TSNE2)$y.values[[1]], 
       col="green", lwd=2)
 
 # Add KPCA with BHV
-lines(attributes(Logit.ROC.MLE.kpca2)$x.values[[1]], attributes(Logit.ROC.MLE.kpca2)$y.values[[1]], 
+lines(attributes(rocs$MLE.kpca2)$x.values[[1]], attributes(rocs$MLE.kpca2)$y.values[[1]], 
       col="brown", lwd=2)
 
 
 #Add Legend
 legend(x=.6, y=.4, c("Direct", "TNSE w/ L2", "KPCA w/ L2", "TNSE w/ BHV", "KPCA w/ BHV"), 
        col=c("black", "red", "blue", "green", "brown"), lwd=c(2,2,2,2,2,2))
-dev.off()
+# dev.off()
 
 #### AUC
-Logit.AUC.MLE.direct
-Logit.AUC.MLE.TSNE1
-Logit.AUC.MLE.kpca1
-Logit.AUC.MLE.TSNE2
-Logit.AUC.MLE.kpca2
+print(aucs$MLE.direct)
+print(aucs$MLE.TSNE1)
+print(aucs$MLE.kpca1)
+print(aucs$MLE.TSNE2)
+print(aucs$MLE.kpca2)
 
+
+## Inferred trees 
+
+
+
+labels = c("callorhinc","danio","gallus","homo",       "latimeria",  "leucoraja",  "lungfish",   "scyliorhin","takifugu", "xenopus"  )
+plot_tree <- function(omega,title){
+  dist_mat = matrix(0,m,m)
+  dist_mat[lower.tri(dist_mat)]= omega - min(omega) 
+  dist_mat = dist_mat + t(dist_mat)
+  row.names(dist_mat) = labels
+  colnames(dist_mat) = labels
+  u = phangorn::upgma(dist_mat)
+  # u = hclust(as.dist(dist_mat),method="complete")
+  # u = as.phylo(u)
+  plot(u,main=title)
+  u
+}
+m=10
+e=45
+titles = names(pars)
+# tree_from_paper = ape::read.tree(text="(((((homo,gallus),xenopus), lungfish),latimeria), (danio, takifugu), ((scyliorhin, leucoraja), callorhinc));")
+tree_from_paper = ape::read.tree(text="((((homo,gallus),xenopus), (lungfish,latimeria)), (danio, takifugu), ((scyliorhin, leucoraja), callorhinc));")
+
+library(phangorn)
+tree_dists = c()
+for(i in 1:10){
+  title = titles[i] 
+  par = pars[i][[title]]
+  omega_1 = par[1:e]
+  # png(paste(title,"_1.png",sep=""))
+  u1= plot_tree(omega_1,title)
+  # dev.off()
+  tree_dists = c(tree_dists,RF.dist(u1,tree_from_paper))
+  
+  if(length(par) > e+2){
+    omega_2 = par[(e+1):(2*e)]
+    # png(paste(title,"_2.png",sep=""))
+    u2= plot_tree(omega_2,title)
+    # dev.off()
+    tree_dists = c(tree_dists,RF.dist(u2,tree_from_paper))
+    # print(RF.dist(u1,u2))
+  }
+}
+
+# res = train_and_test("lungfish/data/lungfish_mle.txt", "lungfish/data/fish_MLE_dbscan_clustering.txt", model="ulr")
+# plot_tree(res$pars[1:e],"dbscan")
+# plot_tree(res$pars[(e+1):(2*e)],"dbscan")
+# res = train_and_test("lungfish/data/lungfish_mle.txt", "lungfish/data/fish_MLE_dbscan_clustering.txt", model="tlr")
+# plot_tree(res$pars[1:e],"dbscan")
