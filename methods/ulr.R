aca322b902ec231f1ef3d7ba04d94b75faa6ec8a
GeorgiosAliatimis <g.aliatimis@lancaster.ac.uk>
2023-05-22 12:31:47 +0100

    bug fix

--- methods/ulr.R
+++ methods/ulr.R
@@ -1,5 +1,4 @@
 source("methods/generic_logistic_regression.R")
-source("methods/upgma.R")
 
 prob <- function(pars,u){
     sigmoid(inner_product(pars,u))
@@ -11,7 +10,7 @@ logistic_gradient <- function(pars,Y,D){
     N = dim(D)[1]
     lambda = pars[length(pars)]
     omega_1 = pars[1:e]
-    omega_2 = pars[(e+1):2*e]
+    omega_2 = pars[(e+1):(2*e)]
     ans = rep(0,length(pars))
     for(i in 1:N){
         i1_min = which.min(D[i,] - omega_1)
@@ -26,7 +25,7 @@ logistic_gradient <- function(pars,Y,D){
         for(i in c(i2_min,i1_max)){
             ans[i] <- ans[i] - (Y[i] - p) * lambda
         }
-        ans[length(pars)] <- ans[length(pars)] + (Y[i] - p) * u
+        ans[length(pars)] <- ans[length(pars)] + (Y[i] - p) * u/lambda
     }
     ans
 }
@@ -38,16 +37,14 @@ trop_dist <- function(x,y)  {
 inner_product <- function(pars,u){
   e = (length(pars) - 1)/2
   lambda = pars[length(pars)]
-  omega_1 = upgma(pars[1:e])
-  omega_2 = upgma(pars[(e+1):(2*e)])
+  omega_1 = pars[1:e]
+  omega_2 = pars[(e+1):(2*e)]
   lambda * ( trop_dist(omega_1,u) - trop_dist(omega_2,u) ) 
 }
 
 normalize_data <- function(D) D
 
 pars_gen <- function(Y,D){
-    N = dim(D)[1]/2
-    # c(colMeans(D[1:N,]), colMeans(D[(N+1):(2*N),]),1.0)
     c(colMeans(D[Y==0,]), colMeans(D[Y==1,]), 1.0)
 }
 
