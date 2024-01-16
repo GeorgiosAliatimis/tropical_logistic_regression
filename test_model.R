dc21e8128f36a87f0ea294a140f13290ab847644
Aliatimis <aliatimi@vdi-stori-053.lancs.local>
2024-01-16 17:32:51 +0000

    test_model deleted, moved to tlr.R and clr.R files

--- test_model.R
+++
@@ -1,9 +0,0 @@
-Testing.Model <- function(pars, D){
-    ## omega is the normal vector for the best-fit Stiefel tropical hyperplane
-    ## D is a nxe matrix.  the ith row is the vector for the predictors for the ith obs.
-    N <- dim(D)[1]
-    Y.hat <- rep(0, N)
-    for(i in 1:N)
-        Y.hat[i] <- prob(pars, D[i,])
-    return(Y.hat)
-}
