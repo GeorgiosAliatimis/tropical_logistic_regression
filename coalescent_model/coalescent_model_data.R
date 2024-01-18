90a6c8a68aa0f44c86e1c1881993f9b70b86eef5
Aliatimis <aliatimi@vdi-stori-053.lancs.local>
2024-01-18 15:05:29 +0000

    get average aucs over many different examples (species trees instances) and train/test splits

--- coalescent_model/coalescent_model_data.R
+++ coalescent_model/coalescent_model_data.R
@@ -1,40 +1,40 @@
 rm(list=ls())
 source("train_and_test.R")
 source("load_data.R")
-mode="classical" # tropical or classical for logistic regression
+mode="tropical" # tropical or classical for logistic regression
 library(phangorn)
 
-Rs = c(seq(0.2,2,0.2), seq(4,10,2))
+dir_name = "coalescent_data"
 e = 45
 m = 10
 
-Rs = c(0.1,1,10,0.2,0.4,0.6,0.8,1.2,1.4,1.6,1.8,2,4,6,8)
+# number of times to run logistic regression on the same dataset, with different train/test splits
+reps_per_example = 10 
+Rs = sort(as.numeric(gsub("Depth","",list.files(dir_name))))
+num_examples = length(list.files(paste(dir_name,"/Depth",min(Rs),sep=""),patter="gene_trees")) %/% 2
 
-ROCs=list()
-AUCs=list()
-probs=list()
-for (R in Rs){
-  dir = paste("coalescent_model/tree_sims/data/Depth",R,sep="")
-  reps = 1
-  #reps = length(list.files(dir,patter="gene_trees")) %/% 2
-  for(r in 1:reps){
-    print(r)
-    S1 = load_tree(paste(dir,"/species_tree_",2*r-1,".nex",sep=""))[[1]]
-    S2 = load_tree(paste(dir,"/species_tree_",2*r,".nex",sep=""))[[1]]
-    
-    gene_trees_file1 = paste(dir,"/",list.files(dir,pattern=paste("gene_trees_",2*r-1,sep=""))[1],sep="")
-    gene_trees_file2 = paste(dir,"/",list.files(dir,pattern=paste("gene_trees_",2*r,sep=""))[1],sep="")
+total_reps = reps_per_example * num_examples
+
+AUCs=matrix(rep(0,total_reps * length(Rs)),nrow=length(Rs),ncol=total_reps)
+for (i in 1:length(Rs)){
+  R = Rs[i]
+  dir = paste(dir_name,"/Depth",R,sep="")
+  aucs = c()
+  for(r in 1:num_examples){
+    print(paste(R,r))
+    S1 = load_tree(paste(dir,"/species_tree_",r,"_1.nex",sep=""))[[1]]
+    S2 = load_tree(paste(dir,"/species_tree_",r,"_2.nex",sep=""))[[1]]
+    gene_trees_file1 = paste(dir,"/gene_trees_",r,"_1.nex", sep="")
+    gene_trees_file2 = paste(dir,"/gene_trees_",r,"_2.nex", sep="")
     
     D1 = load_data(gene_trees_file1)
     D2 = load_data(gene_trees_file2)
     
-    res <- train_and_test(D1,D2,mode=mode)
-    ROCs[[paste(R)]] = res$ROC
-    AUCs[[paste(R)]] = res$AUC
-    probs[[paste(R)]] = res$probs
-    omega_1 = res$pars[1:e]
-    omega_2 = res$pars[(e+1):(2*e)]
-    
-    print(paste(R,res$AUC))
+    aucs = c(aucs,sapply(1:reps_per_example,function(x) train_and_test(D1,D2,mode=mode)$AUC))
   }
+  print(aucs)
+  AUCs[i,] = aucs
 }
+
+library(MASS)
+write.matrix(AUCs,file="aucs.csv",sep=",")
--- coalescent_model/tree_sims/coalescent_data_generation.py
+++ coalescent_model/tree_sims/coalescent_data_generation.py
@@ -4,22 +4,26 @@ import dendropy
 import os 
 
 rng = random.Random(1)
-Rs = [.1,.2,.4,.6,.8,1,1.2,1.4,1.6,1.8,2,4,6,8,10]
-num_species_trees = [1000 if R in [.1,1,10] else 2 for R in Rs]
-num_gene_trees = 1000 
+Rs = [.1,.2,.3,.4,.6,.8,1,1.2,1.4,1.6,1.8,2,4,6,8,10]
+num_instances = 50
+num_species_trees = 2 
+num_gene_trees = 300 
 
-dir_name = "data"
+dir_name = "coalescent_data"
 if dir_name not in os.listdir("."):  
     os.mkdir(dir_name)
 
-for R,N in zip(Rs,num_species_trees):
-    print(R)
-    if f"Depth{R}" not in os.listdir(dir_name):
-        os.mkdir(f"{dir_name}/Depth{R}")
-    for ind in range(N):
-        sp_tree = species_tree(ntax=10,tree_depth = R,rng=rng)
-        gene_trees = [gene_tree_from_species_tree(sp_tree) for _ in range(num_gene_trees)] 
-        gene_trees = dendropy.TreeList(gene_trees)
-        sp_tree = scale_tree(sp_tree)
-        sp_tree.write(path=f"{dir_name}/Depth{R}/species_tree_{ind+1}.nex",schema = "nexus")
-        gene_trees.write(path=f"{dir_name}/Depth{R}/gene_trees_{ind+1}.nex", schema = "nexus")
+for R in Rs:
+	print(R)
+	if f"Depth{R}" not in os.listdir(dir_name):
+		os.mkdir(f"{dir_name}/Depth{R}")
+
+	for ind1 in range(num_instances):
+		print(ind1)
+		for ind2 in range(num_species_trees):
+			sp_tree = species_tree(ntax=10,tree_depth = R,rng=rng)
+			gene_trees = [gene_tree_from_species_tree(sp_tree) for _ in range(num_gene_trees)] 
+			gene_trees = dendropy.TreeList(gene_trees)
+			sp_tree = scale_tree(sp_tree)
+			sp_tree.write(path=f"{dir_name}/Depth{R}/species_tree_{ind1+1}_{ind2+1}.nex",schema = "nexus")
+			gene_trees.write(path=f"{dir_name}/Depth{R}/gene_trees_{ind1+1}_{ind2+1}.nex", schema = "nexus")
