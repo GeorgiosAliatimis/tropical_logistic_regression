312fecd46182791a78735e8f48802fd2c0a3a722
Aliatimis <aliatimi@vdi-stori-053.lancs.local>
2024-01-19 14:58:43 +0000

    delete unnecessary files

--- mr_bayes/primates.sh
+++
@@ -1,3 +0,0 @@
-./routine.sh primates.nex primates_10_4000 16000 10 4000 35 100
-./routine.sh primates.nex primates_10_2000 16000 10 2000 25 100
-./routine.sh primates.nex primates_10_1000 16000 10 1000 10 100
